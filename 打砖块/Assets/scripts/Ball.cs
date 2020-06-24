54d55288dd1a4e7aced1c2d4d6838817b2a94307
ChenJG <dlvguo@qq.com>
2020-06-24 13:50:18 +0800

    打砖块

---
+++ 打砖块/Assets/scripts/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public float ballspeed ;
+    public Rigidbody rb;
+    private bool ballinpaly=false;
+    public GameObject ball;
+	// Use this for initialization
+	void Start() {
+        rb =GetComponent<Rigidbody>();
+        ball = GameObject.Find("qiu");
+           }
+	void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag.Equals("zhuantou"))
+        {
+            Destroy(col.gameObject);
+        }
+        if (col.gameObject.tag == "gameover")
+        {
+            Destroy(ball.gameObject);
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump") && ballinpaly == false)
+        {
+            transform.parent = null;
+            ballinpaly = true;
+            rb.AddForce(new Vector3(45, 45,0)*ballspeed);
+        }
+  
+	}
+}
+     
---
+++ 打砖块/Assets/scripts/Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move: MonoBehaviour {
+    public float xpos,ypos,zpos;
+    public float movespeed;
+    public float value;
+    // Use this for initialization
+	void Start () {
+        xpos = transform.position.x;
+        ypos = transform.position.y;
+        zpos = transform.position.z;	}
+	
+	// Update is called once per frame
+	void Update () {
+        xpos = Input.GetAxisRaw("Horizontal")*movespeed+transform.position.x;
+        xpos=Mathf.Clamp(xpos, -value, value);
+        transform.position = new Vector3(xpos, ypos, zpos);
+
+	}
+}
