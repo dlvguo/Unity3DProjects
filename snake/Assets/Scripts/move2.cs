ee95650bc5d9d72271f32dc60a8455038212c4b1
ChenJG <dlvguo@qq.com>
2020-06-24 13:50:39 +0800

    贪吃蛇

---
+++ snake/Assets/Scripts/move2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move2 : MonoBehaviour {
+    // Use this for initialization
+    public int type;
+	void Start () {
+        type = gamemanger.bodynum;
+        gamemanger.Q.Add(transform.position);
+        InvokeRepeating("move", 0.5f, 0.5f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+    }
+    void move()
+    {
+        transform.position = gamemanger.Q[type - 1];
+        gamemanger.Q[type] = transform.position;
+
+    }
+}
